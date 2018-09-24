882e7a7f0be5bb68c511d4053590b440a933a52e
Andrew Ferguson <andrewmf@umich.edu>
2018-09-24 17:06:27 -0500

    added seed chaining

--- models.js
+++ models.js
@@ -1,38 +1,29 @@
-const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/plantr',
-{logging:false
-})
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/plantr", {
+  logging: false
+});
 
+const Gardener = db.define("gardener", {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+});
 
+const Plot = db.define("plot", {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+});
 
-const Gardener = db.define(
-    'gardener', {
-    name: Sequelize.STRING,
-    age: Sequelize.INTEGER
-    }
-)
+const Vegetable = db.define("vegetable", {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+});
 
-const Plot = db.define(
-    'plot', {
-        size: Sequelize.INTEGER,
-        shaded: Sequelize.BOOLEAN
-    }
-)
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+Vegetable.belongsToMany(Plot, { through: "vegetable_plot" });
+Plot.belongsToMany(Vegetable, { through: "vegetable_plot" });
 
-const Vegetable = db.define(
-    'vegetable', {
-        name: Sequelize.STRING,
-        color: Sequelize.STRING,
-        planted_on: Sequelize.DATE
-    }
-)
+Gardener.belongsTo(Vegetable, { as: "favorite_vegetable" });
 
-
-Plot.belongsTo(Gardener)
-Gardener.hasOne(Plot)
-Vegetable.belongsToMany(Plot, {through: "vegetable_plot"})
-Plot.belongsToMany(Vegetable, {through:"vegetable_plot"})
-
-Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
-
-module.exports = db
+module.exports = db;
--- seed.js
+++ seed.js
@@ -1,25 +1,62 @@
-const db = require('./models')
-const {vegetable} = db.models
-console.log(db.models)
+const db = require("./models");
+const { vegetable, plot, gardener } = db.models;
+
 db.sync({ force: true })
-    .then(() => {
-        console.log('Database synced!')
-        // db.close() // only if using a version of node without `finally`
-    })
-    .then(() =>  
-    {return vegetable.bulkCreate(
-        [{
-            name: 'Carrot',
-            color: 'Orange',
-            planted_on: new Date()
-        }]    
-    )}
-    )
-    .catch(err => {
-        console.log('Disaster! Something went wrong! ')
-        console.log(err)
-        // db.close() // only if using a version of node without `finally`
-    })
-    .finally(() => { // only if using a version of node WITH `finally`
-        db.close()
-    })
+  .then(() => {
+    console.log("Database synced!");
+  })
+  .then(() => {
+    vegetable.bulkCreate([
+      {
+        name: "Carrot",
+        color: "Orange",
+        planted_on: new Date()
+      },
+      {
+        name: "Tomato",
+        color: "Red",
+        planted_on: new Date()
+      }
+    ]);
+  })
+  .then(() => {
+    return vegetable.findAll();
+  })
+  .then(v => {
+    gardener.bulkCreate([
+      {
+        name: "Jeff",
+        age: 23,
+        favoriteVegetableId: v[0].id
+      },
+      {
+        name: "Chris",
+        age: 54,
+        favoriteVegetableId: v[1].id
+      }
+    ]);
+  })
+  .then(() => {
+    return gardener.findAll();
+  })
+  .then((g) => {
+    return plot.bulkCreate([
+      {
+        size: 300,
+        shaded: true,
+        gardenerId: g[0].id
+      },
+      {
+        size: 50,
+        shaded: false,
+        gardenerId: g[1].id
+      }
+    ]);
+  })
+  .catch(err => {
+    console.log("Disaster! Something went wrong! ");
+    console.log(err);
+  })
+  .finally(() => {
+    db.close();
+  });
